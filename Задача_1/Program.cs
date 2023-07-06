00755c8e6165849d29701113cfd3b1ac58669c08
unknown <dtroitskyi@gmail.comgit config --global user.name 'Denis>
2023-07-06 14:23:33 +0300

    Сделано

---
+++ Задача_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int rez = Convert.ToInt32(Console.ReadLine());
+    return rez;
+}
+
+Console.Clear();
+Console.WriteLine("Введите два числа:  ");
+int rezult=1;
+int a = GetNumber("Число А ");
+int b = GetNumber("Число B ");
+for (int i = 0; i < b; i++)
+{
+    rezult *= a;
+}
+Console.WriteLine($"Натуральная степень ->  {rezult}");
---
+++ Задача_2/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int rez = Convert.ToInt32(Console.ReadLine());
+    return rez;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int rezult = 0;
+int a = GetNumber(" ");
+while (a > 0)
+{
+    rezult += a % 10;
+    a = a / 10;
+}
+Console.WriteLine($"Сумма чисел: {rezult}");
